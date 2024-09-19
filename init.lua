f0a2fb18de63d88c3a5aa050ed80785c809b8615
shivam <shivamtrivedi908@gmail.com>
2024-09-19 13:43:51 +0000

    Updates fews things

--- init.lua
+++ init.lua
@@ -6,3 +6,11 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+  group = vim.api.nvim_create_augroup('remove-whitespace-line-end', { clear = true }),
+  pattern = '*',
+  command = [[%s/\s\+$//e]],
+})
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
