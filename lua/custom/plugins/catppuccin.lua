c9fb374bb6b1095178bfff8b77584d97b50ca915
shivam <shivamtrivedi908@gmail.com>
2025-05-07 20:09:12 +0530

    automated dev commit

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -5,6 +5,13 @@ return {
     name = 'catppuccin',
     priority = 1000,
     config = function()
+      require('catppuccin').setup {
+        transparent_background = true,
+        integrations = {
+          -- Enable or disable transparency in specific plugins if needed
+          -- e.g. for telescope, lualine, etc.
+        },
+      }
       vim.cmd.colorscheme 'catppuccin-mocha'
     end,
   },
